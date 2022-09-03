3e44f5875c2387a1edb5703b20c46b8748b5d391
typefoxes <77068559+typefoxes@users.noreply.github.com>
2022-09-04 01:06:45 +0300

    Delete BlackStarProject directory

--- BlackStarProject/DataLoads/DataSubcategory.swift
+++
@@ -1,9 +0,0 @@
-import Foundation
-import RealmSwift
-
-struct DataAllSub: Hashable,Codable {
-    let title: String
-    
-enum CodingKeys: String, CodingKey { case title = "title" }
-}
-class saveItSub: Object { @objc dynamic var title: String? = "" }
--- BlackStarProject/DataLoads/Realm.swift
+++
@@ -1,8 +0,0 @@
-import RealmSwift
-
-class ProductList: Object {
-    @objc dynamic var image = " "
-    @objc dynamic var name = " "
-    @objc dynamic var price = " "
-    
-}
